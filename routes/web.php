421b6f4cd41c9aee9f993a808ddd556f6acfe67d
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2024-08-20 06:39:32 +0700

    feat: add laravel livewire and tailwindcss
    Signed-off-by: Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', HomePage::class)->name('home');
 Route::get('/login', [AuthController::class, 'login'])->name('login');
